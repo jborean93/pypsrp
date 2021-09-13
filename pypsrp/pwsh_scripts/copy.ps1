80a187a576037592eca80cb5d3dbc95d393c1ef1
Malthe Borch <mborch@gmail.com>
2021-09-13 13:41:13 +0200

    Move file, instead of copy/delete
    Note that we always delete afterwards in case of an error.

--- pypsrp/pwsh_scripts/copy.ps1
+++ pypsrp/pwsh_scripts/copy.ps1
@@ -119,10 +119,11 @@ begin {
             throw "Transport failure, hash mismatch`r`nActual: $actual_hash`r`nExpected: $expected_hash"
         }
 
-        # Copy the temp file to the actual dest location and return the absolute path back to the client.
-        [System.IO.File]::Copy($path, $output_path, $true)
+        # Move the temp file to the actual dest location and return the absolute path back to the client.
+        [System.IO.File]::Move($path, $output_path, $true)
         $dest.FullName
     } finally {
+        # Note: If the file to be deleted does not exist, no exception is thrown.
         [System.IO.File]::Delete($path)
     }
 }
